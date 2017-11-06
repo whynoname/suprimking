bcaa857a2b89f640d4d510116c4aff52d00b601e
whynoname <33414272+whynoname@users.noreply.github.com>
2017-11-06 14:38:22 +0545

    Add files via upload

---
+++ header.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta http-equiv="x-ua-compatible" content="ie=edge">
+          <!-- ================== SEO ================= -->
+          <title></title>
+          <meta content="Grand Hotel is among the best hotel accommodation available in Nepal providing modern amenities, quality service, value for money and convenience at affordable rates" name="description" />
+          <meta content="Grand Hotel Official Site, Kathmandu Hotels, Hotels in Kathmandu, Budget hotel in Kathmandu, Luxury Hotel in Kathmandu, Hotel in Nepal, Nepali hotel, accommodation in Kathmandu, accommodation in Nepal, Grand Hotel Official Site" name="keywords" />
+          <meta content="Suprim-Themes" name="author" />
+          <!-- ================== FAVICON ================= -->
+          <link rel="icon" href="" />
+          <!-- ================== STYLESHEETS ================= -->
+          <link rel="Stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+          <link rel="stylesheet" href="assets/bootstrap/datepicker/css/bootstrap-datepicker.min.css">
+          <link rel="Stylesheet" href="assets/css/style.css" />
+          <link rel="Stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css" />
+          <link rel="Stylesheet" href="assets/wow/animate.css" />
+          <!-- ================== GOOGLE FONTS ========== -->
+          <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900%7cRaleway:400,500,600,700" rel="stylesheet">
+
+   </head>
+  <body>
+          <!-- ================== PRELOADER ========== -->
+          <!-- <div id="loading">
+              <div class="inner">
+                  <div class="loading_effect">
+                      <div class="object" id="object_one"></div>
+                      <div class="object" id="object_two"></div>
+                      <div class="object" id="object_three"></div>
+                  </div>
+              </div>
+          </div> -->
+
+          <!-- ================== TOP MENU ========== -->
+          <div class="top_menu transparent hidden-xs">
+              <div class="container">
+                  <ul class="top_menu_right">
+                      <li><i class="fa  fa-phone"></i><a href="tel:18475555555"> 1-888-123-4567 </a></li>
+                      <li class="email"><i class="fa  fa-envelope-o "></i> <a href="">contact@site.com</a></li>
+                      <li class="language-switcher">
+                          <nav class="dropdown">
+                              <a href="#" class="dropdown-toggle select" data-hover="dropdown" data-toggle="dropdown">
+                                  <i class="famfamfam-flag-gb "></i>English
+                              </a>
+                              <ul class="dropdown-menu">
+                                  <li><a href="#"><i class="famfamfam-flag-gr"></i>Ελληνικά</a></li>
+                                  <li><a href="#"><i class="famfamfam-flag-it "></i>Italiano</a></li>
+                                  <li><a href="#"><i class="famfamfam-flag-de "></i>Deutsch</a></li>
+                                  <li><a href="#"><i class="famfamfam-flag-fr "></i>Français</a></li>
+                                  <li><a href="#"><i class="famfamfam-flag-es "></i>Español</a></li>
+                              </ul>
+                          </nav>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+
+          <!-- ================= HEADER ========== -->
+          <header class="fixed transparent">
+            <div class=" addshrink container-fluid">
+              <nav class="navbar navbar-expand-lg  navbar-dark">
+                <div class="container">
+                  <!-- Brand/logo -->
+                  <a class="navbar-brand" href="#"><span class="blueadd">Grand</span><span class="gold"> Hotel</span></a>
+                  <!-- Links -->
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home<b class="caret"></b></a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                    <li class="nav-item"><a class="nav-link"  href="#">Dinning</a></li>
+                    <!-- Dropdown -->
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        Rooms
+                      </a>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                      </div>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Contact us</a>
+                    </li>
+                  </ul>
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Book now</button>
+                </div>
+            </nav>
+            </div>
+          </header>
+          <!-- ================= HERO IMAGE ========== -->
+          <div class="hero_wrapper" id="heroImage">
+            <div class="hero_content">
+               <div class="container">
+                   <div class="row">
+                     <!-- ======== LAYER-1 ============= -->
+                     <div class="col col-8">
+                       <h1 class="text-left hero_title">Welcome to Grand Hotel, Nepali cuisine</h1>
+                       <h3 class="hero_sub"><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i> Star Luxury Hotel</h3>
+                       <ul class="hero_features">
+                          <li class="">
+                            <i class="fa fa-check-square-o"></i> ModernRooms & Spacious Suites
+                          </li>
+                          <li class="">
+                            <i class="fa fa-check-square-o"></i> A Gourmet Dinning experince
+                          </li>
+                          <li class="">
+                            <i class="fa fa-check-square-o"></i> Family Room From $100 per night
+                          </li>
+                       </ul>
+                       <ul class="list-inline fac">
+                         <li  class="row">
+                           <div class="col-2">
+                             <i class="fa fa-wifi fa-small"></i>
+                           </div>
+                           <div class="col">
+                             <p class="">
+                               Enjoy Free Wifi
+                               In the Whole Hotel
+                             </p>
+                           </div>
+                         </li>
+                         <li  class="row">
+                           <div class="col-2">
+                             <i class="fa fa-wifi "></i>
+                           </div>
+                           <div class="col">
+                             <p class="">
+                               Enjoy Free Wifi
+                               In the Whole Hotel
+                             </p>
+                           </div>
+                         </li>
+                         <li  class="row">
+                           <div class="col-2">
+                             <i class="fa fa-wifi "></i>
+                           </div>
+                           <div class="col">
+                             <p class="">
+                               Enjoy Free Wifi
+                               In the Whole Hotel
+                             </p>
+                           </div>
+                         </li>
+                       </ul>
+                     </div><!-- col-8 -->
+                     <!-- ======== LAYER-2 ============= -->
+                     <div class="col col-4">
+                        <div class="bookingf">
+                          <h2 class="form_title"><i class="fa fa-calendar"></i> BOOK ONLINE</h2>
+                          <form id="booking-form" class="inner">
+                             <div class="form-group">
+                                 <input class="form-control" name="booking-name" placeholder="Enter Your Name" type="text" required>
+                             </div>
+                             <div class="form-group">
+                                 <input class="form-control" name="booking-email" placeholder="Enter Your Email Address" type="email" required>
+                             </div>
+                             <div class="form-group">
+                                 <input class="form-control" name="booking-phone" placeholder="Enter Your Phone Number" type="phone" required>
+                             </div>
+                             <div class="form-group">
+                                 <div class="form_select">
+                                     <select name="booking-roomtype" class="form-control" title="Select Room Type" data-header="Room Type" required>
+                                         <option value="Single Room">Single Room</option>
+                                         <option value="Double Room">Double Room</option>
+                                         <option value="Deluxe Room">Deluxe Room</option>
+                                     </select>
+                                 </div>
+                             </div>
+                             <div class="form-group">
+                               <div id="datepicker" class="input-group input-daterange" required>
+                                   <input  type="text" class="form-control" value="Arrival Date" required>
+                                   <!-- <div class="input-group-addon">to</div> -->
+                                   <input type="text" class="form-control" value="Depature Date" required>
+                               </div>
+                             </div>
+                             <button class="button btn_lg btn_blue btn_full" type="submit">BOOK A ROOM NOW</button>
+                             <div class="a_center mt10">
+                                 <a href="booking-form.html" class="a_b_f">Advanced Booking Form</a>
+                             </div>
+                         </form>
+                        </div>
+                     </div><!-- col-4 -->
+                   </div>
+                 </div>
+                <a href="#rooms"><i  class="fa fa-angle-down fa-2x hero_down"></i></a>
+             </div>
+           </div>
+           <!-- ================ FAVORITE ROOMS ========== -->
+           <section class="white_bg" id="rooms">
+               <div class="container">
+
+                   <div class="main_title mt_wave a_center">
+                       <h2>OUR FAVORITE ROOMS</h2>
+                   </div>
+                   <p class="main_description a_center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.</p>
+
+                   <div class="row">
+                           <div class="col-md-4">
+                               <article class="room">
+                                   <figure>
+                                       <div class="price">€89 <span>/ night</span></div>
+                                       <a class="hover_effect h_blue h_link" href="room.html">
+                                           <img src="images/rooms/xsingle-room.jpg.pagespeed.ic.inoyU7NRmP.jpg" class="img-responsive" alt="Image">
+                                       </a>
+                                       <figcaption>
+                                           <h4><a href="room.html">Single Room</a></h4>
+                                           <span class="f_right"><a href="rooms-list.html" class="button btn_sm btn_blue">VIEW DETAILS</a></span>
+                                       </figcaption>
+                                   </figure>
+                               </article>
+                           </div>
+                           <div class="col-md-4">
+                               <article class="room">
+                                   <figure>
+                                       <div class="price">€129 <span>/ night</span></div>
+                                       <a class="hover_effect h_blue h_link" href="room.html">
+                                           <img src="images/rooms/xdouble-room.jpg.pagespeed.ic.iiS1TV5TYH.jpg" class="img-responsive" alt="Image">
+                                       </a>
+                                       <figcaption>
+                                           <h4><a href="room.html">Double Room</a></h4>
+                                           <span class="f_right"><a href="room.html" class="button btn_sm btn_blue">VIEW DETAILS</a></span>
+                                       </figcaption>
+                                   </figure>
+                               </article>
+                           </div>
+                           <div class="col-md-4">
+                               <article class="room">
+                                   <figure>
+                                       <div class="price"> € 189 <span>/ night</span></div>
+                                       <a class="hover_effect h_blue h_link" href="room.html">
+                                           <img src="images/rooms/xdeluxe-room.jpg.pagespeed.ic.0up5xG7rE6.jpg" class="img-responsive" alt="Image">
+                                       </a>
+                                       <figcaption>
+                                           <h4><a href="room.html">Delux Room</a></h4>
+                                           <span class="f_right"><a href="room.html" class="button btn_sm btn_blue">VIEW DETAILS</a></span>
+                                       </figcaption>
+                                   </figure>
+                               </article>
+                           </div>
+                       </div>
+                       <div class="mt40 a_center">
+                           <a class="button btn_sm btn_yellow" href="rooms-list.html">VIEW ROOMS LIST</a>
+                       </div>
+               </div>
+           </section>
+    <script src="assets/bootstrap/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <script src="assets/bootstrap/js/popper.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/bootstrap/datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script src="assets/js/jquery.easing.1.3.js"></script>
+    <script src="main.js"></script>
+  </body>
+</html>
